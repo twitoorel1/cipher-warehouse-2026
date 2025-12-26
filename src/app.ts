15ec565fe275d190d4e7189d8822ae5207dbc598
twitoorel1 <twitoorel1@Gmail.com>
2025-12-26 15:29:54 +0200

    updated app.ts

--- src/app.ts
+++ src/app.ts
@@ -32,6 +32,7 @@ export const createApp = (env: AppEnv, pool: Pool) => {
 
   app.use(rateLimit());
   app.use(express.json({ limit: "1mb" }));
+  app.use(express.urlencoded({ extended: true, limit: "1mb" }));
   app.use(cookieParser());
 
   app.use(createAuthRouter(pool, env));
